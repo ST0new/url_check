63b396bff4abfa20322f7d0c5ed2bb8ff0d70a89
ST0new <45659593+ST0new@users.noreply.github.com>
2022-07-04 11:44:51 +0800

    Update url_check.py
    更新0.3

--- url_check.py
+++ url_check.py
@@ -1,15 +1,17 @@
 import random
-import requests
 import sys
+import argparse
+from concurrent.futures import ThreadPoolExecutor
 from urllib.parse import urlparse
-import re
-import threading, queue
 
+import requests
+from bs4 import BeautifulSoup
+import os
+import threading
+import re
 requests.packages.urllib3.disable_warnings()
-url_queue = queue.Queue()
-threading_num = 50
-count = 0
-survival_urls = {}
+
+count=0
 useragent_list = [
     "Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.8.1) Gecko/20061010 Firefox/2.0",
     "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/3.0.195.6 Safari/532.0",
@@ -22,8 +24,6 @@ useragent_list = [
     "Mozilla/5.0 (Windows; U; Windows NT 6.0; fr-FR) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16",
     "Mozilla/5.0 (Windows; U; Windows NT 6.0; fr-FR) AppleWebKit/533.18.1 (KHTML, like Gecko) Version/5.0.2 Safari/533.18.5"
 ]
-
-
 def Banner():
     print("""
                _   _ ____  _     ____   ____ _               _    
@@ -31,101 +31,88 @@ def Banner():
               | | | | |_) | |   \___ \| |   | '_ \ / _ \/ __| |/ /
               | |_| |  _ <| |___ ___) | |___| | | |  __/ (__|   < 
                \___/|_| \_\_____|____/ \____|_| |_|\___|\___|_|\_\
-
-                                                Power by ST0new  v0.2
+                                                Power by ST0new  v0.3
     """)
 
 
-def get_headers(url):
+
+def get_headers(url):  #
     res = urlparse(url)
-    headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'User-Agent': random.choice(useragent_list),
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.8',
-        "Referer": res.scheme + "://" + res.netloc,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    return headers
+    header = {"User-Agent": random.choice(useragent_list),
+              "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", \
+              "Referer": res.scheme + "://" + res.netloc,
+              "Content-Type": "application/x-www-form-urlencoded"
+              }
+    return header
 
 
-def get_url(urls_txt):
-    with open(urls_txt, 'r') as f:
-        urls_list = f.readlines()
-    return urls_list
+def tech_detect():  # wappalyzer 指纹识别
+    # api key O9EwJN51d86HHRRjyH95H2r3o5jIaE6G7kt5sT5j
+    pass
 
 
-def url_check():
-    global survival_urls
-    while not url_queue.empty():
+def check_url(url):
+    # 检查url是否有schema
+    url = url.strip("\n")
+    if ("http://" or "https://") not in url:
+        url = "http://" + url
+    return url
 
-        global count
-        url = url_queue.get()
+def get_pageTitle(response):
 
-        url = url.strip()
-        headers = get_headers(url)
-        print(url)
-        try:
-            
-#            if "http" not in url and len(url) != 0  :
-#                url = "http://"+ url
-            response = requests.get(url, headers=headers, timeout=5,verify=False)
-            survival_urls[url] = response.status_code
-            if 200 <= response.status_code <= 206:
-                response.encoding = "utf-8"
-                content = response.text
-                title = re.findall('<title>(.+)</title>', content)
-                if title:
-                    print(url + "\t" + str(response.status_code) + "\t" + title[0])
+    # 获取页面title
+    try:
+        # soup = BeautifulSoup(response.text, 'html.parser')
+        # pagetitle = soup.find("title")
+        pagetitle = re.findall("(?<=\<title\>)(?:.|\n)+?(?=\<)", response.text, re.IGNORECASE)[0].strip()
+    except:
+        pagetitle = '无法访问'
+    return pagetitle
 
-            count += 1
-            print("已检测：" + str(count), end="\r")
+def get_domainInfo(url):
+    global count
+    try:
+        url = check_url(url)
+        response = requests.get(url=url, headers=get_headers(url), timeout=5)
+        response.encoding = 'utf-8'
+        #print(response.text)
+        result =  response.url+"    "+ str(response.status_code)+"    "+ str(get_pageTitle(response))
+        count += 1
+        print("[%d]" % count,result)
 
-            url_queue.task_done()
+        put_file(result)
+    except Exception as e:
+        pass
 
-        except Exception as e:
-            print(e)
+def put_file(result):  # 将结果写入result.txt中
+    with open("result.txt",'a+') as f:
+        f.writelines(result+"\n")
 
+def get_urls(file):
+    with open(file,'r',encoding='utf-8') as f:
+        urls = f.readlines()
+        return urls
 
-
-def write_url():
-    with open("result.txt", "w+") as f:
-        for url, status_code in survival_urls.items():
-            f.write(url + "\t" + str(status_code) + "\n")
-
-def threading_start(urls_list):
-    for url in urls_list:
-        url = url.strip()
-        if len(url) == 0:
-            continue
-        url_queue.put(url)
-    threads = []
-    for _ in range(10):
-        c = threading.Thread(target=url_check)
-        threads.append(c)
-        c.setDaemon(True)
-    for t in threads:
-        t.start()
-
-    url_queue.join()
-
-
-def urls_check(urls_txt):
-    urls_list = get_url(urls_txt)
-
-    threading_start(urls_list)
-
-    write_url()
-
-
-
+def parser():  # 创建命令行参数
+    parser = argparse.ArgumentParser(description='获取URL信息')
+    parser.add_argument('-f', '--file', default='domain.txt', help='从txt文件中读取域名')
+    parser.add_argument('-u', '--url',default=None,type=str, help='获取单个域名信息')
+    args = parser.parse_args()
+    return args
+def multi_threading(urls):
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for urls in urls:
+            executor.submit(
+                put_file, url=urls
+            )
 
 if __name__ == '__main__':
-    Banner()
-
-    try:
-        urls_txt = sys.argv[1]
-    except:
-        print("python3 check.py url.txt")
-    urls_check(urls_txt)
-# 注意： url的格式要是http://xx.com
+    args = parser()  # 从command 获取的参数
+    if args.url:
+        urls = args.url
+        get_domainInfo(urls)
+    elif args.file and os.path.exists(args.file):
+        urls = get_urls(args.file)
+        for url in urls:
+            multi_thread = threading.Thread(target=get_domainInfo, args=(url,))
+            multi_thread.start()
